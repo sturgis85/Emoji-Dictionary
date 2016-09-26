7cf9856e00946614c695370cb6069d75d92ba466
John Fiedler <doscattos@gmail.com>
2016-09-26 19:13:19 -0400

    Added a tableview with sample icon and label

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,14 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet var dacooltableview: UITableView!
+    
+    var emojis = ["😎","🐰","🍒","🎥","💙","#️⃣"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("STARTING")
+        dacooltableview.dataSource = self
+        dacooltableview.delegate = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // answer number of rows (duh!)
+        return emojis.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // answer what we want inside each row
+        let cell = UITableViewCell() // 
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
