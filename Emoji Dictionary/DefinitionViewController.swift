204e56c6756c4d27cf1a242fb4306ee50e752d24
John Fiedler <doscattos@gmail.com>
2016-10-04 14:48:39 -0400

    Passing emoji to new controller... everything done except the explanations

---
+++ Emoji Dictionary/DefinitionViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DefinitionViewController.swift
+//  Emoji Dictionary
+//
+//  Created by John Fiedler on 10/3/16.
+//  Copyright © 2016 SlobberHound. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+    
+    var emoji = "NO EMOJI"
+
+    @IBOutlet var emojiLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        emojiLabel.text = emoji
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
